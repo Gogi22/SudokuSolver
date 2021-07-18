6f3f381bd57dfe4c58aeb3e9282e8f2f1059e659
Gogi <gogimamutchadze@gmail.com>
2021-07-18 17:02:20 +0400

    added keypad

--- board.js
+++ board.js
@@ -1,40 +1,75 @@
-const sudoku = $('.sudoku')
-
-let grid = ''
-for(let i=1; i<=9;i++){
-    let row = '"'
-    for(let j=1; j<=9; j++){
-        square=$(document.createElement('div'))
-        if (i == 1){
-            square.addClass('top-big') 
+const generateSudoku = () => {
+    let grid = ''
+    for(let i=1; i<=9;i++){
+        let row = '"'
+        for(let j=1; j<=9; j++){
+            square=$(document.createElement('div'))
+            if (i == 1){
+                square.addClass('top-big') 
+            }
+            if (i%3 == 0){
+                square.addClass('bottom-big') 
+            }
+            else{
+                square.addClass('bottom')
+            }
+            if (j == 9){
+                square.addClass('right-big') 
+            }
+            if (j%3 == 1){
+                square.addClass('left-big') 
+            }
+            else{
+                square.addClass('left')
+            }
+            
+            let n = `box-${i}-${j}`
+            square.addClass('box') 
+            square.addClass(n)
+            square.css('gridArea', n)
+            sudoku.append(square)
+            row += n + ' '
         }
-        if (i%3 == 0){
-            square.addClass('bottom-big') 
-        }
-        else{
-            square.addClass('bottom')
-        }
-        if (j == 9){
-            square.addClass('right-big') 
-        }
-        if (j%3 == 1){
-            square.addClass('left-big') 
-        }
-        else{
-            square.addClass('left')
-        }
-        
-        let n = `box-${i}-${j}`
-        square.addClass('box') 
-        square.addClass(n)
-        square.css('gridArea', n)
-        sudoku.append(square)
-        row += n + ' '
+        row += '"'
+        grid += row
     }
-    row += '"'
-    grid += row
+    sudoku.css('grid-template-areas', grid)
 }
-sudoku.css('grid-template-areas', grid)
+
+const generateControls = () => {
+    for(let i=1; i<=9; i++){
+        let key = $(document.createElement('div'))
+        let keyName = `key-${i}` 
+        key.text(i)
+        key.addClass(keyName)
+        key.addClass('key')
+        key.css('grid-area', keyName)
+        controls.append(key)
+    }
+
+    let delet = $(document.createElement('div'))
+    let restart = $(document.createElement('div'))
+    let check = $(document.createElement('div'))
+    let answer = $(document.createElement('div'))
+    delet.addClass('key delete')
+    restart.addClass('key restart')
+    check.addClass('key check')
+    answer.addClass('key answer')
+    delet.css('gridArea', 'delete')
+    restart.css('gridArea', 'restart')
+    check.css('gridArea', 'check')
+    answer.css('gridArea', 'answer')
+    delet.text("Delete")
+    restart.text("Restart")
+    check.text("Check")
+    answer.text("Reveal the Answer")
+    controls.append(delet, restart, check, answer)
+}
+
+const sudoku = $('.sudoku')
+const controls = $('.controls')
+generateSudoku()
+generateControls()
 
 $('.box-3-5').text('5')
 $('.box-9-9').text('9')
@@ -54,6 +89,11 @@ $(document).on('keypress', e => {
         $(selectedBox).text(String.fromCharCode(e.which))
         $(selectedBox).css('color', numColor)
     }
+})
+
+$(document).on('keydown', e => {
+    if((e.which == 8 || e.which == 46) && !$(selectedBox).hasClass('pre-set'))
+        $(selectedBox).text("")
 })
 
 let sudokuGrid = [[ 3, 0, 6, 5, 0, 8, 4, 0, 0 ],
