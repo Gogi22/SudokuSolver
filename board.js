4abdf628f92fd3a4b7c7f68cb89d3f8dae189232
Gogi <gogimamutchadze@gmail.com>
2021-07-18 21:23:34 +0400

    added keypad functionality

--- board.js
+++ board.js
@@ -1,4 +1,4 @@
-const generateSudoku = () => {
+const generateSudokuLayout = () => {
     let grid = ''
     for(let i=1; i<=9;i++){
         let row = '"'
@@ -66,51 +66,104 @@ const generateControls = () => {
     controls.append(delet, restart, check, answer)
 }
 
+const generateSudoku = () => {
+    let sudokuGrid = [[ 3, 0, 6, 5, 0, 8, 4, 0, 0 ],
+                      [ 5, 2, 0, 0, 0, 0, 0, 0, 0 ],
+                      [ 0, 8, 7, 0, 0, 0, 0, 3, 1 ],
+                      [ 0, 0, 3, 0, 1, 0, 0, 8, 0 ],
+                      [ 9, 0, 0, 8, 6, 3, 0, 0, 5 ],
+                      [ 0, 5, 0, 0, 9, 0, 6, 0, 0 ],
+                      [ 1, 3, 0, 0, 0, 0, 2, 5, 0 ],
+                      [ 0, 0, 0, 0, 0, 0, 0, 7, 4 ],
+                      [ 0, 0, 5, 2, 0, 6, 3, 0, 0 ] ]
+    
+    for(let i = 0; i < 9; i++){
+        for(let j = 0; j < 9; j++){
+            if(sudokuGrid[i][j]){
+                $(`.box-${i+1}-${j+1}`).text(sudokuGrid[i][j])
+                $(`.box-${i+1}-${j+1}`).addClass('pre-set')
+            }
+        }
+    }
+}
+
+
+const sudokuReset = () => {
+    for(let i = 0; i < 81; i++){
+        if(!$(sudoku.children()[i]).hasClass('pre-set')) 
+            $(sudoku.children()[i]).text("")
+    }
+
+    // add timer reset
+}
+
 const sudoku = $('.sudoku')
 const controls = $('.controls')
-generateSudoku()
-generateControls()
-
-$('.box-3-5').text('5')
-$('.box-9-9').text('9')
-
 let selectedBox = null;
-const numColor = 'rgb(29,106,229)'
+
+generateSudokuLayout()
+generateControls()
+generateSudoku()
+
 
 $('.box').on('click', e => {
     let toggle = $(e.currentTarget).hasClass('select-box');
     $('.box').removeClass('select-box')
     $(e.currentTarget).toggleClass('select-box', !toggle)
     selectedBox = !toggle ? e.currentTarget : null
+    selectedBox = $(e.currentTarget).hasClass('pre-set') ? null : selectedBox
 })
 
 $(document).on('keypress', e => {
-    if(e.which >= 49 && e.which <= 57 && !$(selectedBox).hasClass('pre-set')){
+    if(e.which >= 49 && e.which <= 57){
         $(selectedBox).text(String.fromCharCode(e.which))
-        $(selectedBox).css('color', numColor)
     }
 })
 
 $(document).on('keydown', e => {
-    if((e.which == 8 || e.which == 46) && !$(selectedBox).hasClass('pre-set'))
-        $(selectedBox).text("")
+    if((e.which == 8 || e.which == 46))
+    $(selectedBox).text("")
+    
+    // reset restart
+    $('.restart').text('Restart')
+    $('.restart').removeClass('restart-selected')
 })
 
-let sudokuGrid = [[ 3, 0, 6, 5, 0, 8, 4, 0, 0 ],
-                  [ 5, 2, 0, 0, 0, 0, 0, 0, 0 ],
-                  [ 0, 8, 7, 0, 0, 0, 0, 3, 1 ],
-                  [ 0, 0, 3, 0, 1, 0, 0, 8, 0 ],
-                  [ 9, 0, 0, 8, 6, 3, 0, 0, 5 ],
-                  [ 0, 5, 0, 0, 9, 0, 6, 0, 0 ],
-                  [ 1, 3, 0, 0, 0, 0, 2, 5, 0 ],
-                  [ 0, 0, 0, 0, 0, 0, 0, 7, 4 ],
-                  [ 0, 0, 5, 2, 0, 6, 3, 0, 0 ] ]
-
-for(let i = 0; i < 9; i++){
-    for(let j = 0; j < 9; j++){
-        if(sudokuGrid[i][j]){
-            $(`.box-${i+1}-${j+1}`).text(sudokuGrid[i][j])
-            $(`.box-${i+1}-${j+1}`).addClass('pre-set')
-        }
+$(document).on('click', (e) => {
+    // reset restart if clicked outside
+    if($(e.target)[0]!==$('.restart')[0]){
+        $('.restart').text('Restart')
+        $('.restart').removeClass('restart-selected')
     }
-}
+})
+
+$('.key').on('click', e => {
+    let keyClass = $(e.currentTarget).attr('class').split(/\s+/)[0];
+    $(selectedBox).text(keyClass.substring(keyClass.length -1))
+})
+
+$('.delete').on('click', (e) => {
+    $(selectedBox).text("")
+})
+
+$('.restart').on('click', () => {
+    if($('.restart').css('color') === 'rgb(255, 255, 255)'){
+        $('.restart').text('Restart')
+        $('.restart').toggleClass('restart-selected')
+        sudokuReset()
+    }
+    else{
+        $('.restart').text('Confirm?')
+        $('.restart').toggleClass('restart-selected')
+    }
+})
+
+
+$('.check').on('click', () => {
+    // check();
+})
+
+$('.answer').on('click', () => {
+    // solve();
+})
+
